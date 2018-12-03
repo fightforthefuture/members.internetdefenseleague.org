07408688b532e531c80976fb51d5a1e136ea811c
Ken Mickles <ken@37i.net>
2018-12-03 16:48:06 -0500

    add include.js

---
+++ include.js
@@ -0,0 +1,7 @@
+// (function() {
+//   var _fftf_widget = document.createElement('script');
+//   _fftf_widget.type = 'text/javascript';
+//   _fftf_widget.async = true;
+//   _fftf_widget.src = 'https://redalert.battleforthenet.com/widget.js';
+//   document.getElementsByTagName('body')[0].appendChild(_fftf_widget);
+// })();
